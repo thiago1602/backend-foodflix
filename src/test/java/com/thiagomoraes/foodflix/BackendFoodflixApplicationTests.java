389692fa51eac0702114f5e98bfb376460cb1bac
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-03-25 22:51:58 -0300

    Project create

---
+++ src/test/java/com/thiagomoraes/foodflix/BackendFoodflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagomoraes.foodflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFoodflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
