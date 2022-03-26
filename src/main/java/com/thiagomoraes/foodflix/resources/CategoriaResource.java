9885ca4319cb6acae799c07066e3a411b459d927
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-03-26 19:46:36 -0300

    Class dominio Category

--- src/main/java/com/thiagomoraes/foodflix/resources/CategoriaResource.java
+++ src/main/java/com/thiagomoraes/foodflix/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.thiagomoraes.foodflix.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thiagomoraes.foodflix.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
