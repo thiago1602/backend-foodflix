d8e5bab197b93ae52f04068a343d68c6bfc06e88
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-03-28 15:15:01 -0300

    Create operation instacie

--- src/main/java/com/thiagomoraes/foodflix/BackendFoodflixApplication.java
+++ src/main/java/com/thiagomoraes/foodflix/BackendFoodflixApplication.java
@@ -1,13 +1,30 @@
 package com.thiagomoraes.foodflix;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BackendFoodflixApplication {
+import com.thiagomoraes.foodflix.domain.Categoria;
+import com.thiagomoraes.foodflix.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class BackendFoodflixApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(BackendFoodflixApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
