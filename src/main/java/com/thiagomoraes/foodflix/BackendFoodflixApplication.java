8713977b6cba5d223501362a2d33f36f751c17f4
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-03-28 16:57:29 -0300

    Create table product and association many to many

--- src/main/java/com/thiagomoraes/foodflix/BackendFoodflixApplication.java
+++ src/main/java/com/thiagomoraes/foodflix/BackendFoodflixApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.thiagomoraes.foodflix.domain.Categoria;
+import com.thiagomoraes.foodflix.domain.Produto;
 import com.thiagomoraes.foodflix.repositories.CategoriaRepository;
+import com.thiagomoraes.foodflix.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class BackendFoodflixApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendFoodflixApplication.class, args);
 	}
@@ -24,7 +29,19 @@ public class BackendFoodflixApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList( p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
