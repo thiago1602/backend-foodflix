890c289947441265ced8e3c9a83baec63246bd3e
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-03-29 18:42:37 -0300

    inserindo nova categoria usando o post

--- src/main/java/com/thiagomoraes/foodflix/resources/CategoriaResource.java
+++ src/main/java/com/thiagomoraes/foodflix/resources/CategoriaResource.java
@@ -1,14 +1,15 @@
 package com.thiagomoraes.foodflix.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.thiagomoraes.foodflix.domain.Categoria;
 import com.thiagomoraes.foodflix.services.CategoriaService;
@@ -25,5 +26,13 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> Insert(@RequestBody Categoria obj){
+		obj = service.Insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();		
 	}
 }
