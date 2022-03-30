b95d3a0a59fdba5b68b85ff64a14392688008c21
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-03-30 15:18:18 -0300

    listando categorias com dto

---
+++ src/main/java/com/thiagomoraes/foodflix/dto/CategoriaDTO.java
@@ -0,0 +1,48 @@
+package com.thiagomoraes.foodflix.dto;
+
+import java.io.Serializable;
+
+import com.thiagomoraes.foodflix.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+
+	private Integer id;
+	private String nome;
+	
+	
+	public CategoriaDTO() {
+		
+	}
+
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
--- src/main/java/com/thiagomoraes/foodflix/resources/CategoriaResource.java
+++ src/main/java/com/thiagomoraes/foodflix/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.thiagomoraes.foodflix.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.thiagomoraes.foodflix.domain.Categoria;
+import com.thiagomoraes.foodflix.dto.CategoriaDTO;
 import com.thiagomoraes.foodflix.services.CategoriaService;
 
 @RestController
@@ -51,4 +54,14 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	
+	@RequestMapping( method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).
+				collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		
+	}
+	
 }
