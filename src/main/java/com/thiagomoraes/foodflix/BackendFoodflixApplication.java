5401fafd516ba1107e5ddfbdd24ebbecb77f9490
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-03-30 16:29:11 -0300

    paginacao

--- src/main/java/com/thiagomoraes/foodflix/BackendFoodflixApplication.java
+++ src/main/java/com/thiagomoraes/foodflix/BackendFoodflixApplication.java
@@ -61,6 +61,11 @@ public class BackendFoodflixApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
+		Categoria cat3 = new Categoria(null,"Lanches");
+		Categoria cat4 = new Categoria(null,"Pizzas");
+		Categoria cat5 = new Categoria(null,"Refrigerantes");
+		Categoria cat6 = new Categoria(null,"Porções");
+		Categoria cat7 = new Categoria(null,"Sucos");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -73,7 +78,7 @@ public class BackendFoodflixApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
