411a9a7dd70b0077ccc0f6f08d37ee2c721a224f
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-03-31 16:32:34 -0300

    busca por produtos nome e categoria

--- src/main/java/com/thiagomoraes/foodflix/services/validation/utils/ClientInsert.java
+++ src/main/java/com/thiagomoraes/foodflix/services/validation/utils/ClientInsert.java
@@ -4,8 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 import javax.validation.Constraint;
 import javax.validation.Payload;
+
+import com.thiagomoraes.foodflix.services.validation.ClientInsertValidator;
 
 @Constraint(validatedBy = ClientInsertValidator.class)
 @Target({ ElementType.TYPE })
