7e1c8f60ea3a1168c0dc203f9fbdb19cf4ae4753
Thiago Moraes <thiagomoraesf95@gmail.com>
2022-04-01 16:44:51 -0300

    Implementatando tosTRING DE PEDIDO

--- src/main/java/com/thiagomoraes/foodflix/domain/Pedido.java
+++ src/main/java/com/thiagomoraes/foodflix/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.thiagomoraes.foodflix.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -19,6 +22,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Pedido implements Serializable {
+	
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -134,6 +139,25 @@ public class Pedido implements Serializable {
 		return true;
 	}
 
-		
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy hh:MM:ss");
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido número=");
+		builder.append(getId());
+		builder.append(", Instante: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		for (ItemPedido ip : getItens()) {
+			builder.append(ip.toString());
+		}
+		builder.append("Valor total: ");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();
+	}
 		
 }
--- src/main/java/com/thiagomoraes/foodflix/services/PedidoService.java
+++ src/main/java/com/thiagomoraes/foodflix/services/PedidoService.java
@@ -11,6 +11,7 @@ import com.thiagomoraes.foodflix.domain.ItemPedido;
 import com.thiagomoraes.foodflix.domain.PagamentoComBoleto;
 import com.thiagomoraes.foodflix.domain.Pedido;
 import com.thiagomoraes.foodflix.domain.enums.EstadoPagamento;
+import com.thiagomoraes.foodflix.repositories.ClientRepository;
 import com.thiagomoraes.foodflix.repositories.ItemPedidoRepository;
 import com.thiagomoraes.foodflix.repositories.PagamentoRepository;
 import com.thiagomoraes.foodflix.repositories.PedidoRepository;
@@ -34,34 +35,35 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClientService clientService;
 	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 	
-	@Transactional
-	public Pedido Insert(Pedido obj) {
+	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setClient(clientService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
-		
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		for(ItemPedido ip : obj.getItens() ) {
+		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
-		
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
